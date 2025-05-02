633582d0c517f8970bfe9c6b6f4ef90ad174afd9
ht3aa <ht3aa2001@gmail.com>
2025-05-02 15:24:43 +0300

    Update has-relations view with improved styling and table structure

--- resources/views/has-relations.blade.php
+++ resources/views/has-relations.blade.php
@@ -1,25 +1,29 @@
-<div>
-    @foreach($templates as $template)
-        <div class="mb-4">
-            <h3 class="text-lg font-medium">{{ $template['label'] }}</h3>
-            <table class="w-full">
-                <thead>
-                    <tr>
-                        @foreach($template['columns'] as $column)
-                            <th class="px-4 py-2 text-left">{{ $column }}</th>
+<div >
+        @foreach ($templates as $template)
+            <div>
+                <h3 class="text-lg font-medium leading-6 text-gray-900" style="margin-bottom: .8rem; margin-top: 2rem;">{{ $template['label'] }}</h3>
+                <x-filament-tables::container  style="overflow: auto; max-height: 300px; ">
+                    <x-filament-tables::table>
+                        @foreach ($template['columns'] as $column)
+                            <x-filament-tables::header-cell :wire:key="$template['relation'] . '-' . $column">
+                                {{ $column }}
+                            </x-filament-tables::header-cell>
                         @endforeach
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($template['tableRecords'] as $record)
-                        <tr>
-                            @foreach($template['columns'] as $column)
-                                <td class="px-4 py-2">{{ $record[$column] ?? '' }}</td>
+                    @foreach ($template['tableRecords'] as $record)
+                        <x-filament-tables::row >
+                            @foreach ($template['columns'] as $column)
+                                <x-filament-tables::cell>
+                                    <p style="padding: 12px 16px;">
+                                        {{ $record[$column] }}
+                                    </p>
+                                </x-filament-tables::cell>
                             @endforeach
-                        </tr>
+                        </x-filament-tables::row>
                     @endforeach
-                </tbody>
-            </table>
-        </div>
-    @endforeach
-</div> 
+
+                    </x-filament-tables::table>
+
+                </x-filament-tables::container>
+            </div>
+        @endforeach
+</div>
